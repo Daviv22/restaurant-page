502b452234a77a734a7f98c62008f4b5682759ca
Davi Vieira Vitorino <davivvitorino@gmail.com>
2026-05-29 18:03:36 -0300

    Configura rotas

---
+++ src/routes.jsx
@@ -0,0 +1,17 @@
+import Home from "./app/pages/Home";
+import Menu from "./app/pages/Menu";
+import Contact from "./app/pages/Contact";
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./app/components/Layout";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout />,
+        children: [
+            { path: '/', element: <Home /> },
+            { path: 'menu', element: <Menu /> },
+            { path: 'about', element: <Contact /> },
+        ],
+    },
+]);
